736ce8e52d9a8beffb38bb2ea5e0d56c9cb55cf1
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-06 14:24:25 +0700

    Add ListTransactionCard

---
+++ src/components/molecule/ListCard/ListTransactionCard.tsx
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { color } from '../../../theme'
+import { widthResponsive } from '../../../utils/dimensionFormat'
+import { Transactions } from '../../../interface/transaction.interface'
+import { Gap } from '../../atom'
+
+interface Props {
+  item: Transactions;
+  onPress: ({item}: {item: Transactions}) => void;
+}
+
+const ListTransactionCard = ({ item, onPress }: Props) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => onPress({item})}>
+      <View style={[styles.cardLeftStyle, {backgroundColor: item.status.toString() === 'SUCCESS' ? color.Success[400] : color.Warning[700]}]} />
+      <View style={styles.mainBodyContainer}>
+        <Text style={[styles.textStyle, {fontWeight: 'bold'}]}>
+          {item.sender_bank.toString()}{' -> '} {item.beneficiary_bank.toString()}
+        </Text>
+        <Gap height={5} />
+        <Text style={styles.textStyle}>
+          {item.beneficiary_name.toString()}
+        </Text>
+        <Gap height={5} />
+        <Text style={styles.textStyle}>
+          {item.amount.toString()} {' . '} {item.created_at.toString()}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+export default ListTransactionCard
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: widthResponsive(10),
+    marginBottom: widthResponsive(12),
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: color.Neutral[30],
+  },
+  cardLeftStyle: {
+    width: widthResponsive(10),
+    backgroundColor: color.Primary[500],
+    borderTopLeftRadius: widthResponsive(10),
+    borderBottomLeftRadius: widthResponsive(10),
+  },
+  mainBodyContainer: {
+    flex: 1,
+    padding: widthResponsive(10),
+    backgroundColor: color.Neutral[10],
+    borderTopRightRadius: widthResponsive(10),
+    borderBottomRightRadius: widthResponsive(10),
+  },
+  textStyle: {
+    color: color.Dark[800],
+    fontSize: widthResponsive(16),
+  },
+})
--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,47 +1,92 @@
-import {FlatList, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
+import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {color} from '../theme';
 import {widthResponsive} from '../utils';
 import {useDispatch, useSelector} from 'react-redux';
 import {ApplicationState} from '../interface/redux.interface';
 import {fetchDataRequest} from '../redux/actions/home';
+import store from '../redux/store';
+import { Gap, SearchBar } from '../components/atom';
+import { mvs } from 'react-native-size-matters';
+import { EmptyState } from '../components/molecule';
+import ListTransactionCard from '../components/molecule/ListCard/ListTransactionCard';
+import { Transactions } from '../interface/transaction.interface';
 
 const HomeScreen = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<typeof store.dispatch>();
   const {data, loading, error} = useSelector(
     (state: ApplicationState) => state.home,
   );
+
+  const [searchState, setSearchState] = useState('');
 
   useEffect(() => {
     dispatch(fetchDataRequest());
   }, [dispatch]);
 
-  // Render your UI based on the state
+  const transactionIds = Object.values(data) as Transactions[];
+
+  const onPress = ({item}: {item: Transactions}) => {
+    console.log(item);
+  }
+
+  
   return (
-    <View>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : error ? (
-        <Text>Error: {error}</Text>
-      ) : (
+    <SafeAreaView style={styles.root}>
+    <>
+      <View style={styles.bodyContainer}>
+            <SearchBar
+              value={searchState}
+              onChangeText={(newText: string) => setSearchState(newText)}
+              rightIcon={searchState !== '' && true}
+              reset={() => setSearchState('')}
+            />
+
+        <Gap height={20} />
+
         <FlatList
-          data={data}
-          renderItem={({item}) => <Text>{item.title}</Text>}
-          keyExtractor={item => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+          data={transactionIds}
+          renderItem={({item}) => (
+            <ListTransactionCard item={item} onPress={onPress} />
+          )}
+          ListEmptyComponent={<EmptyState text="No data found" />}
         />
-      )}
-    </View>
+      </View>
+    </>
+    </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: color.Dark[800],
+    backgroundColor: color.Neutral[20],
+  },
+  bodyContainer: {
+    flex: 1,
     padding: widthResponsive(20),
+    paddingHorizontal: widthResponsive(10),
+  },
+  filterContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonStyle: {
+    marginRight: widthResponsive(20),
+  },
+  filterStyle: {
+    aspectRatio: undefined,
+    width: widthResponsive(125),
+    height: widthResponsive(30),
+  },
+  text: {
+    color: color.Dark[50],
+    fontSize: mvs(15),
+    fontFamily: 'Inter-Medium',
   },
 });
