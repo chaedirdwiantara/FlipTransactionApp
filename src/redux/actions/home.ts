3537f81100baf56723ae4b8a4f0debbbb2e31931
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-06 14:25:02 +0700

    Update home.ts action

--- src/redux/actions/home.ts
+++ src/redux/actions/home.ts
@@ -1,10 +1,10 @@
 import { FETCH_DATA_FAILURE, FETCH_DATA_REQUEST, FETCH_DATA_SUCCESS, HomeActionTypes } from "../../interface/redux.interface";
-
+import { Transactions } from "../../interface/transaction.interface";
 export const fetchDataRequest = (): HomeActionTypes => ({
   type: FETCH_DATA_REQUEST,
 });
 
-export const fetchDataSuccess = (data: any[]): HomeActionTypes => ({
+export const fetchDataSuccess = (data: Transactions[]): HomeActionTypes => ({
   type: FETCH_DATA_SUCCESS,
   payload: data,
 });
