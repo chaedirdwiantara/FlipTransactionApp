7290d0dc758270b5dbe6c20f72bbf47535d5fbbd
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-07 19:08:31 +0700

    Update index

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -4,7 +4,8 @@ import RewardsIcon from "./Rewards.icon";
 import ProfileIcon from "./Profile.icon";
 import ArrowRightIcon from "./ArrowRight.icon";
 import CloseIcon from "./Close.icon";
+import CopyIcon from "./Copy.icon";
 
 export {
-    FeedIcon, HomeIcon, RewardsIcon, ProfileIcon, ArrowRightIcon, CloseIcon
+    FeedIcon, HomeIcon, RewardsIcon, ProfileIcon, ArrowRightIcon, CloseIcon, CopyIcon
 }
