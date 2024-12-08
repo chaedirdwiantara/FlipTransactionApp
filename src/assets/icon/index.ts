34d36ae911da64e8ec4fffa8fff6f010ad1e3735
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-08 15:52:38 +0700

    Delete unused icons

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,11 +1,7 @@
-import FeedIcon from "./Feed.icon";
-import HomeIcon from "./Home.icon";
-import RewardsIcon from "./Rewards.icon";
-import ProfileIcon from "./Profile.icon";
 import ArrowRightIcon from "./ArrowRight.icon";
 import CloseIcon from "./Close.icon";
 import CopyIcon from "./Copy.icon";
 
 export {
-    FeedIcon, HomeIcon, RewardsIcon, ProfileIcon, ArrowRightIcon, CloseIcon, CopyIcon
+    ArrowRightIcon, CloseIcon, CopyIcon
 }
