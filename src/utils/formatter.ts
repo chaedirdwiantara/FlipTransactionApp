3df7aaaf82da873c3854c24ecb90cec23b7c2c2c
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-08 17:24:25 +0700

    Delete unused function

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,19 +1,3 @@
-import { Dimensions, Platform, PixelRatio } from 'react-native';
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
-
-// based on iphone 5s's scale
-const scale = SCREEN_WIDTH / 320;
-
-const normalize = (size: number) => {
-  const newSize = size * scale;
-  if (Platform.OS === 'ios') {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize));
-  } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
-  }
-};
-
 const elipsisText = (text: string, lengthText: number): string =>
   text?.length > lengthText ? text.substring(0, lengthText - 3) + '...' : text;
 
@@ -35,8 +19,18 @@ const formatCurrency = (value: number): string => {
 
 function formatDate(dateString: string): string {
   const months: string[] = [
-    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
-    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
   ];
 
   const date = new Date(dateString);
@@ -47,5 +41,4 @@ function formatDate(dateString: string): string {
   return `${day} ${month} ${year}`;
 }
 
-
-export { normalize, elipsisText, capitalizeText, formatCurrency, formatDate };
+export {elipsisText, capitalizeText, formatCurrency, formatDate};
