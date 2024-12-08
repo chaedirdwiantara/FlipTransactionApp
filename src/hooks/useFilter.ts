e0eabc8f9de551e7cfd014a0f6782f9351327de2
chaedir dwiantara <chaedirdwiantara@gmail.com>
2024-12-08 17:22:28 +0700

    Add reusable custom hook for filter

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,26 +1,47 @@
-import { Transactions } from '../interface/transaction.interface';
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {Transactions} from '../interface/transaction.interface';
+import {updateSortedData} from '../redux/actions/transactions.action';
+import store from '../redux/store';
 
-const filterTransactions = (data: Transactions[], selectedFilter: string): Transactions[] => {
-    if (!data) return [];
+const useFilter = (data: Transactions[], selectedFilter: string) => {
+  const dispatch = useDispatch<typeof store.dispatch>();
+
+  useEffect(() => {
+    if (!data) return;
 
     let sortedData = [...data];
     switch (selectedFilter) {
-        case 'Nama A-Z':
-            sortedData.sort((a, b) => a.beneficiary_name?.toString().localeCompare(b.beneficiary_name?.toString()));
-            break;
-        case 'Nama Z-A':
-            sortedData.sort((a, b) => b.beneficiary_name?.toString().localeCompare(a.beneficiary_name?.toString()));
-            break;
-        case 'Tanggal Terbaru':
-            sortedData.sort((a, b) => new Date(b.created_at.toString()).getTime() - new Date(a.created_at.toString()).getTime());
-            break;
-        case 'Tanggal Terlama':
-            sortedData.sort((a, b) => new Date(a.created_at.toString()).getTime() - new Date(b.created_at.toString()).getTime());
-            break;
-        default:
-            sortedData = data;
+      case 'Nama A-Z':
+        sortedData.sort((a, b) =>
+          a.beneficiary_name?.toString().localeCompare(b.beneficiary_name?.toString()),
+        );
+        break;
+      case 'Nama Z-A':
+        sortedData.sort((a, b) =>
+          b.beneficiary_name?.toString().localeCompare(a.beneficiary_name?.toString()),
+        );
+        break;
+      case 'Tanggal Terbaru':
+        sortedData.sort(
+          (a, b) =>
+            new Date(b.created_at.toString()).getTime() -
+            new Date(a.created_at.toString()).getTime(),
+        );
+        break;
+      case 'Tanggal Terlama':
+        sortedData.sort(
+          (a, b) =>
+            new Date(a.created_at.toString()).getTime() -
+            new Date(b.created_at.toString()).getTime(),
+        );
+        break;
+      default:
+        sortedData = data;
     }
-    return sortedData;
+
+    dispatch(updateSortedData(sortedData));
+  }, [selectedFilter]);
 };
 
-export default filterTransactions;
+export default useFilter;
